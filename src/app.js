9ab07b9dbc0f7ef869122a3671dd3984f6a03483
Jure J <jure.jeglic@smartis.si>
2020-10-04 20:44:23 +0200

    added dropdown hook

--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./pet";
+import SearchParams from "./searchParams";
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Besa",
-      animal: "Dog",
-      breed: "Lake Terrier",
-    }),
-    React.createElement(Pet, { name: "Metka", animal: "cat", breed: "tabby" }),
-    React.createElement(Pet, {
-      name: "Lord",
-      animal: "Dog",
-      breed: "German Shepard",
-    }),
-    React.createElement(Pet, {
-      name: "Potato",
-      animal: "Pony",
-      breed: "Lipicanec",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <SearchParams></SearchParams>
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App></App>, document.getElementById("root"));
