002be21307d652a43c414f411afdb9f9fe2b83a6
Jure J <jure.jeglic@smartis.si>
2020-10-11 22:03:50 +0200

    saving progress

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./pet";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./searchParams";
+import Details from "./Details";
+
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams></SearchParams>
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/"></SearchParams>
+          <Details path="/details/:id"></Details>
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 render(<App></App>, document.getElementById("root"));
